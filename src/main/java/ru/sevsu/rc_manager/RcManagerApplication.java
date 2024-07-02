3241c62bfda985f6d7720cb19d607f733a970dac
DarthTapik <darthtapik@yandex.ru>
2024-07-02 13:27:24 +0300

    Initial commit

---
+++ src/main/java/ru/sevsu/rc_manager/RcManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.sevsu.rc_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sevsu/rc_manager/RcManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sevsu.rc_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
