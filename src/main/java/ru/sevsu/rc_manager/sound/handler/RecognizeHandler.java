d43d7c346465f22028ff6e425fef6c57c846b09c
Victor Uss <victoruss97@gmail.com>
2024-07-04 17:02:32 +0300

    Transcription output to log in string format

--- src/main/java/ru/sevsu/rc_manager/sound/handler/RecognizeHandler.java
+++ src/main/java/ru/sevsu/rc_manager/sound/handler/RecognizeHandler.java
@@ -1,12 +1,12 @@
 package ru.sevsu.rc_manager.sound.handler;
 
-import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.*;
-import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,32 +15,28 @@ import org.vosk.LogLevel;
 import org.vosk.Model;
 import org.vosk.Recognizer;
 
+
+
 @Component
 @Slf4j
 public class RecognizeHandler implements Handler {
     private Recognizer recognizer;
+    private ObjectMapper objectMapper;
 
     @PostConstruct
     private void init() throws IOException {
         String modelPath = "/home/user2/Documents/vosk-model-small-ru-0.22";//ссылка выносится в конфиг
         Model model = new Model(modelPath);
         recognizer = new Recognizer(model, 16000);
+        objectMapper = new ObjectMapper();
     }
+
 
     @Override
     public void handle(AudioInputStream audioInputStream) {
         try {
-            LibVosk.setLogLevel(LogLevel.DEBUG);
-
-            // Assuming the path to the model and audio file are known and accessible
+            LibVosk.setLogLevel(LogLevel.WARNINGS);
             String audioFilePath = "temp.wav";
-            /*try {
-                AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE,
-                        new File(audioFilePath));
-            } catch (IOException e) {
-                log.warn(e.getMessage());
-            }
-*/
 
             InputStream ais = AudioSystem.getAudioInputStream(
                     new BufferedInputStream(new FileInputStream(audioFilePath)));
@@ -48,10 +44,11 @@ public class RecognizeHandler implements Handler {
                 byte[] b = new byte[4096];
                 while ((nbytes = ais.read(b)) >= 0){
                     if (recognizer.acceptWaveForm(b, nbytes)) {
-                        System.out.println(recognizer.getResult());
+                        JsonNode jsonNode = objectMapper.readTree(recognizer.getResult());
+                        String text = jsonNode.get("text").asText();
+                        log.info(text);
                     }
                 }
-                System.out.println(recognizer.getFinalResult());
             } catch (UnsupportedAudioFileException | IOException e) {
                 e.printStackTrace();
             }
