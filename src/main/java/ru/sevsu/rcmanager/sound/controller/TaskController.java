f66e3226a78a325f0cb565d0935463e28c75438e
homeUser <homeUser@gmail.com>
2024-07-11 15:21:54 +0300

    added execution of commands even if they differ by a couple of characters, this was done in order to filter out recognition inaccuracies

--- src/main/java/ru/sevsu/rcmanager/sound/controller/TaskController.java
+++ src/main/java/ru/sevsu/rcmanager/sound/controller/TaskController.java
@@ -3,15 +3,19 @@ package ru.sevsu.rcmanager.sound.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.text.similarity.LevenshteinDistance;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class TaskController {
+    @Value("${sound.levenshtein-distance}")
+    private int levenshteinDistance;
     private Map<String, Command> commandsMap = new HashMap<>();
-    private final String[] commands = {"открой ворота","верни стену", "большой брат следит за тобой"};
+    private final String[] commands = {"открой ворота","дуров верни стену", "большой брат следит за тобой"};
     public boolean findString(String[] strings, String desired) {
         for (String str : strings) {
             if (desired.contains(str)) {
@@ -29,27 +33,32 @@ public class TaskController {
         // Инициализация команд
         commandsMap.put("открой ворота", new OpenDoorsCommand());
         commandsMap.put("большой брат следит за тобой", new EightCommand());
-        commandsMap.put("верни стену", new OpenVkCommand());
+        commandsMap.put("дуров верни стену", new OpenVkCommand());
     }
 
     public void executeCommand(String commandName) {
-        if (commandsMap.containsKey(commandName)) {
             Command command = commandsMap.get(commandName);
             command.execute();
             log.info("Команда '{}' выполнена.", commandName);
-        } else {
-            log.debug("Команда '{}' не найдена.", commandName);
-        }
     }
 
     public void searchCommand(String command){
-        if ((command == null) || (command.isBlank())){
+        if ((command == null) || (command.isBlank())) {
             return;
         }
         command = command.toLowerCase();
 
-        if (findString(commands, command)){
-            executeCommand(command);
+        for (String key : commands) {
+            LevenshteinDistance distance = new LevenshteinDistance();
+            int diff = distance.apply(command, key);
+
+            // Проверяем, не превышает ли разница в 2 символа
+            if (diff <= levenshteinDistance) {
+                executeCommand(key);
+                return; // Выходим из метода после выполнения команды
+            }
         }
+
+        log.debug("Команда '{}' не найдена или не соответствует ни одной из команд.", command);
     }
 }
